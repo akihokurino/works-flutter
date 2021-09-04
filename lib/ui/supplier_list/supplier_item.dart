ce7309107d2831f35d0e984c846fb096db5c3e00
akihokurino <aki030402@gmail.com>
2021-09-04 22:30:31 +0900

    supplier list

---
+++ lib/ui/supplier_list/supplier_item.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:works_flutter/infra/graphql/api.dart';
+import 'package:works_flutter/model/supplier.dart';
+import 'package:works_flutter/ui/color.dart';
+import 'package:works_flutter/ui/font.dart';
+
+class SupplierItem extends StatelessWidget {
+  final Supplier supplier;
+  final VoidCallback onClick;
+
+  SupplierItem({required this.supplier, required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: ColorPalette.border, width: 1),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Material(
+        color: supplier.billingType == GraphQLBillingType.monthly
+            ? ColorPalette.primary.withOpacity(0.1)
+            : ColorPalette.okGreen.withOpacity(0.1),
+        child: InkWell(
+          onTap: () {
+            onClick();
+          },
+          child: Container(
+            padding: EdgeInsets.fromLTRB(16, 14, 16, 14),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Icon(
+                    Icons.apartment,
+                    size: 40,
+                    color: ColorPalette.iconGray,
+                  ),
+                  margin: EdgeInsets.only(right: 15),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Text(supplier.name,
+                            style: TextStyle(
+                              color: ColorPalette.text,
+                              fontWeight: Font.normalWeight,
+                              fontSize: 15,
+                            )),
+                        margin: EdgeInsets.only(bottom: 2),
+                      ),
+                      Container(
+                        child: Text("${supplier.billingAmountIncludeTax}（税込）",
+                            style: TextStyle(
+                              color: ColorPalette.text,
+                              fontWeight: Font.boldWeight,
+                              fontSize: 15,
+                            )),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 15, color: ColorPalette.arrowIcon)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/supplier_list/supplier_list.dart
+++ lib/ui/supplier_list/supplier_list.dart
@@ -2,13 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:works_flutter/ui/color.dart';
 import 'package:works_flutter/ui/component/appbar.dart';
-import 'package:works_flutter/ui/supplier_list/provider.dart';
+import 'package:works_flutter/ui/font.dart';
+import 'package:works_flutter/ui/root/provider.dart';
+import 'package:works_flutter/ui/supplier_list/supplier_item.dart';
 
 class SupplierListPage extends HookWidget {
   final GlobalKey<NavigatorState> globalKey;
+  final _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
 
   static Widget init(GlobalKey<NavigatorState> globalKey) {
     return SupplierListPage(globalKey: globalKey);
@@ -18,27 +20,71 @@ class SupplierListPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final state = useProvider(supplierListProvider);
     useEffect(() {}, const []);
-
-    final content = ModalProgressHUD(
-        progressIndicator: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(ColorPalette.primary),
-        ),
-        child: _buildContent(),
-        inAsyncCall: state.shouldShowHud);
 
     return Scaffold(
       key: globalKey,
       backgroundColor: Colors.white,
       appBar: AppBarFactory(title: "取引先").build(context),
-      body: content,
+      body: _buildContent(),
     );
   }
 
   Widget _buildContent() {
+    final state = useProvider(rootProvider);
+    final suppliers = state.me?.suppliers ?? [];
+
+    List<Widget> widgets = [];
+    widgets.add(Container(
+      margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+      child: Column(
+        children: [
+          Container(
+            child: Text("月額の合計額（税込）",
+                style: TextStyle(
+                  color: ColorPalette.text,
+                  fontWeight: Font.normalWeight,
+                  fontSize: 15,
+                )),
+          ),
+          Container(
+            child: Text("${suppliers.map((v) => v.billingAmountIncludeTax).reduce((v1, v2) => v1 + v2)}円",
+                style: TextStyle(
+                  color: ColorPalette.text,
+                  fontWeight: Font.boldWeight,
+                  fontSize: 30,
+                )),
+          )
+        ],
+      ),
+    ));
+
+    widgets.addAll(suppliers.map((v) => Container(
+          margin: EdgeInsets.only(bottom: 15),
+          child: SupplierItem(supplier: v, onClick: () {}),
+        )));
+
     return Container(
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: NotificationListener(
+        child: RefreshIndicator(
+            key: _refreshIndicatorKey,
+            color: ColorPalette.primary,
+            onRefresh: _onRefresh,
+            child: Scrollbar(
+              child: ListView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                children: widgets,
+              ),
+            )),
+        onNotification: (ScrollNotification notification) {
+          return true;
+        },
+      ),
     );
+  }
+
+  Future<void> _onRefresh() async {
+    await Future.delayed(Duration(seconds: 2));
   }
 }
